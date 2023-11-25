89f18f155822fd22f1ad6dc7faa33d0913cae383
DAVID DEBOER <ddeboer@berkeley.edu>
2023-11-25 11:15:51 -0800

    part of name change

---
+++ ddproject/ddproject.py
@@ -0,0 +1,387 @@
+from copy import copy
+from . import plotting
+from . import util
+from ddgantt.components import *
+import datetime
+from argparse import Namespace
+import csv
+
+
+class Project:
+    """
+    This is a collection of components (components.py) and handles them as an aggregate.
+    """
+    chart_types = ['task', 'milestone', 'timeline']
+    event_types = ['task', 'milestone']
+    other_types = ['timeline', 'note']
+
+    def __init__(self, name, organization=None):
+        self.entry_types = self.event_types + self.other_types
+        self.predecessor_types = copy(self.chart_types)
+        self.name = name
+        self.organization = organization
+        self.all_entries = {}
+        for entry_type in self.entry_types:
+            setattr(self, f"{entry_type}s", [])
+        self.colinear_map = {}
+        self.earliest, self.latest = {}, {}
+        self.updated = None
+        for entry in self.entry_types:
+            self.earliest[entry] = None
+            self.latest[entry] = None
+        self.predecessor_timing_flags = {}
+        for pt in self.predecessor_types:
+            self.predecessor_timing_flags[pt] = False
+
+    def __repr__(self):
+        extrema = self._get_event_extrema()
+        duration = extrema.max - extrema.min
+        s = f"Project:  {self.name}\n"
+        if self.organization is not None:
+            s += f"Organization: {self.organization}\n"
+        s += f"{extrema.min} - {extrema.max}  ({duration})\n"
+        tot = 0
+        for et in self.entry_types:
+            n = len(getattr(self, f"{et}s"))
+            if n:
+                pr = '' if n == 1 else 's'
+                s += f"{n:03d} {et.capitalize()}{pr}\n"
+                tot += n
+        s +=  "-----------------\n"
+        s += f"{tot:03d} Total Entries\n"
+        return s
+
+    def add_entry(self, entry):
+        try:
+            if entry.key in self.all_entries.keys():
+                print(f"Warning - not adding '{entry.type}': Key for {entry.name} already used ({entry.key}).")
+                return
+        except AttributeError:
+            return
+        self.all_entries[entry.key] = copy(entry)
+        if entry.type in ['milestone', 'note']:
+            early_date = copy(entry.date)
+            late_date = copy(entry.date)
+        else:
+            early_date = copy(entry.begins)
+            late_date = copy(entry.ends)
+        if self.earliest[entry.type] is None:
+            self.earliest[entry.type] = early_date
+        elif early_date < self.earliest[entry.type]:
+            self.earliest[entry.type] = early_date
+        if self.latest[entry.type] is None:
+            self.latest[entry.type] = late_date
+        elif late_date > self.latest[entry.type]:
+            self.latest[entry.type] = late_date
+        try:
+            if entry.colinear is not None and len(entry.colinear.strip()):
+                self.colinear_map[entry.key] = entry.colinear
+        except AttributeError:
+            pass
+        getattr(self, f"{entry.type}s").append(entry.key)
+        if entry.type in self.predecessor_timing_flags and entry.predecessor_timing:
+            self.predecessor_timing_flags[entry.type] = True
+
+    def _sort_(self, entry_types, sortby):
+        sort_key_dict = {}
+        for entry_type in entry_types:
+            for key in getattr(self, f"{entry_type}s"):
+                this = self.all_entries[key]
+                skey = ''
+                for upar in sortby:
+                    try:
+                        skey += str(getattr(this, upar)) + '_'
+                    except AttributeError:
+                        continue
+                skey += f"{key}"  # To make sure unique
+                sort_key_dict[skey] = this.key
+        sorted_keys = []
+        for skey in sorted(sort_key_dict.keys()):
+            sorted_keys.append(sort_key_dict[skey])
+        return sorted_keys
+
+    def _align_keys(self, ykeys):
+        """
+        Goes through colinear and puts them on the same line as first
+        """
+        mapkeys = []
+        for ykey in ykeys:
+            if ykey in self.colinear_map:
+                mapkeys.append(self.colinear_map[ykey])
+            else:
+                mapkeys.append(ykey)
+        return mapkeys
+
+    def _get_event_extrema(self):
+        chkmin, chkmax = [], []
+        for event in self.chart_types:
+            if self.earliest[event] is not None:
+                chkmin.append(self.earliest[event])
+            if self.latest[event] is not None:
+                chkmax.append(self.latest[event])
+        chkmin = None if not len(chkmin) else min(chkmin)
+        chkmax = None if not len(chkmax) else max(chkmax)
+        return Namespace(min=chkmin, max=chkmax)
+
+    def set_predecessors(self):
+        """
+        Set project predecessor timing.
+        """
+        for pt in self.predecessor_types:
+            timing = []
+            if self.predecessor_timing_flags[pt]:
+                for key in getattr(self, f"{pt}s"):
+                    if self.all_entries[key].predecessor_timing:
+                        for pkey in self.all_entries[key].predecessors:
+                            that = self.all_entries[pkey]
+                            timing.append(that.date if that.type == 'milestone' else that.ends)
+                        self.all_entries[key].set_predecessor_timing(timing)
+
+    def chart(self, chart='all', sortby=['begins', 'date', 'name', 'ends'], interval=None, grid=False, show_weekends=True):
+        """
+        Make a gantt chart.
+
+        Parameter
+        ---------
+        sortby : list or 'all' (chart_types)
+           fields to sort by
+        """
+        self.gantt = plotting.Gantt(name = self.name)
+        self.set_predecessors()
+        if chart == 'all':
+            chart = self.chart_types
+        elif isinstance(chart, str):
+            chart = [chart]
+        dates = []
+        labels = []
+        plotpars = []
+        ykeys = []  # keys lists the keys used, used to make the vertical axis including colinear
+        extrema = self._get_event_extrema()
+        duration = extrema.max - extrema.min
+        print(f"Duration = {util.pretty_duration(duration.total_seconds())}")
+        for sortkey in self._sort_(chart, sortby):
+            this = self.all_entries[sortkey]
+            if this.type == 'milestone':
+                dates.append([this.date, None])
+                plotpars.append(Namespace(color=this.get_color(), marker=this.marker, owner=this.owner))
+            elif this.type == 'timeline':
+                dates.append([this.begins, this.ends])
+                plotpars.append(Namespace(color=this.get_color(), status=None, owner=None))
+            elif this.type == 'task':
+                dates.append([this.begins, this.ends])
+                plotpars.append(Namespace(color=this.get_color(), status=this.status, owner=this.owner))
+            if this.label is not None:
+                labels.append(this.label)
+            else:
+                labels.append(this.name)
+            ykeys.append(this.key)
+        ykeys = self._align_keys(ykeys)
+        self.gantt.setup(dates=dates, plotpars=plotpars, labels=labels, ykeys=ykeys, extrema=extrema)
+        self.gantt.chart(interval=interval, grid=grid, show_weekends=show_weekends)
+
+    def cumulative(self, step=1.0, show=True):
+        """
+        Make a cumulative milestone chart.
+
+        Parameter
+        ---------
+        sortby : list
+           fields to sort by, must be unique.  sort_info dicts map if needed
+        """
+        extrema = self._get_event_extrema()
+        dates, status = [], []
+        for key in self.milestones + self.tasks:
+            this = self.all_entries[key]
+            dates.append(this.date if this.type == 'milestone' else this.ends)
+            status.append(Namespace(status=this.status, type=this.type))
+        self.cdf = Namespace(dates=[], values=[])
+        this_date = copy(extrema.min)
+        while this_date < extrema.max:
+            self.cdf.dates.append(this_date)
+            ctr = 0.0
+            for i in range(len(dates)):
+                if this_date > dates[i] and self._eval_status_complete(status[i]):
+                    ctr += 1.0
+            self.cdf.values.append(ctr)
+            this_date += datetime.timedelta(days=step)
+        if self.cdf.dates[-1] != extrema.max:
+            self.cdf.dates.append(extrema.max)
+            ctr = 0.0
+            for i in range(len(status)):
+                if self._eval_status_complete(status[i]):
+                    ctr += 1.0
+            self.cdf.values.append(ctr)
+        if show:
+            plotting.cumulative_graph(self.cdf.dates, self.cdf.values, len(dates))
+
+    def _eval_status_complete(self, status):
+        if isinstance(status.status, str) and status.status.lower() == 'complete':
+            return True
+        try:
+            score = int(status.status)
+        except (TypeError, ValueError):
+            score = False
+        if score == 100:
+            return True
+        return False
+
+    def show_notes(self, sortby=['date', 'jot']):
+        sorted_keys = self._sort_(['note'], sortby)
+        for sortkey in sorted_keys:
+            this = self.all_entries[sortkey]
+            print(f"{this.jot}  {this.date.strftime('%Y-%m-%d %H:%M')}  - ({', '.join(this.reference)})")
+
+    def color_bar(self):
+        util.color_bar()
+
+    def _determine_entry_type(self, header, row):
+        kwargs = {}
+        for hdr, r in zip(header, row):
+            if len(hdr.strip()):
+                for h in hdr.split(':'):
+                    kwargs[h.strip()] = r.strip()
+        if 'type' in kwargs:
+            if isinstance(kwargs['type'], str) and kwargs['type'].lower() in self.entry_types:
+                return kwargs['type']
+        for trial in ['task', 'timeline', 'milestone', 'note']:
+            if self.empty_classes[trial].valid_request(**kwargs):
+                return trial
+        return False
+
+    def _preproc_val(self, hdr, val):
+        """
+        Do more later, e.g. check if val=='none' and return None etc...
+
+        """
+        if isinstance(val, str):  # Should _always_ be a str
+            val = val.strip()
+            if not len(val):
+                return val
+        else:
+            print(f"Information:  {val} is not a str ({hdr}) - do I care?")
+        if hdr == 'colinear':
+            if val.startswith('#'):
+                val = val.strip('#')
+            else:
+                val = self.empty_classes['entry'].make_key(val)
+        return val
+
+    def csvread(self, loc, verbose=False):
+        fp = None
+        print(f"Reading {loc}")
+        self.empty_classes = components_dict()  # defined in components.py
+        classdecl = {'milestone': Milestone, 'timeline': Timeline, 'task': Task}
+
+        if loc.startswith('http'):
+            data = util.load_sheet_from_url(loc)
+            header = copy(data[0])
+            reader = data[1:]
+        else:
+            fp = open(loc, 'r')
+            reader = csv.reader(fp)
+            header = next(reader)
+        
+        for row in reader:
+            entry_type = self._determine_entry_type(header, row)
+            if not entry_type:
+                print(f"No valid entry_type:  {row}")
+                continue
+            kwargs = {}
+            for hdrc, val in zip(header, row):
+                for hdr in hdrc.split(':'):
+                    if hdr.strip() in self.empty_classes[entry_type].parameters:
+                        kwargs[hdr] = self._preproc_val(hdr, val)
+                        break
+                    elif verbose:
+                        print(f"{hdr} not valid component.")
+            if self.empty_classes[entry_type].valid_request(**kwargs):
+                if verbose:
+                    print(f'Adding {entry_type}  {row}')
+                if entry_type == 'note':
+                    jot = copy(kwargs['jot'])
+                    del kwargs['jot']
+                    this = Note(jot=jot, **kwargs)
+                else:
+                    name = copy(kwargs['name'])
+                    del kwargs['name']
+                    this = classdecl[entry_type](name=name, **kwargs)
+                self.add_entry(this)
+            elif verbose:
+                print(f"Skipping invalid {entry_type}:  {row}.")
+        if fp is not None:
+            fp.close()
+
+    def scriptwrite(self, fn='export_script.py', projectname='project'):
+        """
+        This will write out the project to a python script.
+        """
+        print(f"Writing {fn}")
+        ctr = {}
+        with open(fn, 'w') as fp:
+            print("from ddgantt import ddproject as ddp\n", file=fp)
+            org = '' if self.organization is None else f", organization='{self.organization}'"
+            print(f"{projectname} = gantt.Project('{self.name}'{org})\n", file=fp)
+            for entry in self.all_entries.values():
+                ctr[entry.type].setdefault(1)
+                print(entry.gen_script_entry(ctr[entry.type], projectname), file=fp)
+                ctr[entry.type] += 1
+
+    def _get_csv_col(self, paired_col):
+        """Done ugly to get the complete and unique column headers."""
+        cols = []
+        trackcol = set()
+        pcdict = {}
+        for pcol in paired_col:
+            col2 = pcol.split(':')
+            pcdict[col2[0]] = col2[1]
+            pcdict[col2[1]] = col2[0]
+            pcdict[f"v{col2[0]}"] = pcol
+            pcdict[f"v{col2[1]}"] = pcol
+
+        entpar = util.components_parameters(show=False)
+        for entry in self.entry_types:
+            for p in entpar[entry]:
+                if p in pcdict:
+                    trackcol.add(pcdict[p])
+                if p not in trackcol:
+                    if p in pcdict:
+                        cols.append(pcdict[f'v{p}'])
+                    else:
+                        cols.append(p)
+                trackcol.add(p)
+#        cols = ['name', 'begins:date', 'ends', 'duration', 'colinear', 'color', 'status', 'groups', 'label',
+#                'complete', 'marker', 'note:jot', 'owner', 'predecessors:reference', 'updated', 'type', 'key']
+        return cols
+
+    def csvwrite(self, fn, paired_col=['begins:date', 'note:jot', 'predecessors:reference']):
+        print(f"Writing csv file {fn}")
+        ccols = self._get_csv_col(paired_col=paired_col)
+        return ccols
+        with open(fn, 'w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(ccols)
+            for entry in ['milestones', 'timelines', 'tasks', 'notes']:
+                for this in getattr(self, entry).values():
+                    row = []
+                    for cols in ccols:
+                        added = False
+                        for col in cols.split(':'):
+                            try:
+                                val = getattr(this, col)
+                                if val is None:
+                                    val = ''
+                                elif col in util.DATE_FIELDS:
+                                    val = util.datedeltastr(val)
+                                elif col in util.LIST_FIELDS:
+                                    val = '|'.join([str(_x) for _x in val])
+                                row.append(val)
+                                added = True
+                                break
+                            except AttributeError:
+                                if col == 'type':
+                                    row.append(entry.strip('s'))
+                                    added = True
+                        if not added:
+                            row.append('')
+                    writer.writerow(row)
+
---
+++ ddproject/plotting.py
@@ -0,0 +1,206 @@
+"""
+Creates a simple Gantt chart
+Adapted from https://bitbucket.org/DBrent/phd/src/1d1c5444d2ba2ee3918e0dfd5e886eaeeee49eec/visualisation/plot_gantt.py  # noqa
+BHC 2014
+
+Adapted by ddeboer 6/Feb/2015
+Re-adapted 2023 August 26
+Re-re-adapted 2023 October 23 - made into class
+"""
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates
+import numpy as np
+from ddgantt.util import color_palette
+from copy import copy
+
+
+class Gantt:
+    def __init__(self, name=None):
+        self.name = name
+
+    def setup(self, dates, plotpars, labels, ykeys, extrema):
+        """
+        Parameters
+        ----------
+        dates : list
+            each entry is a datetime pair (or end is None)
+        plotpars : list
+            list of Namespaces containing the data for the chart
+        labels : list
+            extra labels for entries
+        ykeys : list
+            entry keys contained within the plot
+        extrema : Namespace
+            min/max datetimes
+        """
+        self.dates = dates
+        self.plotpars = plotpars
+        self.labels = labels
+        self.ykeys = ykeys
+        self.extrema = extrema
+        self.datemin, self.datemax = matplotlib.dates.date2num(extrema.min), matplotlib.dates.date2num(extrema.max)
+        self.deltadate = self.datemax - self.datemin
+
+    def plot_weekends(self, color='lightcyan'):
+        """
+        Include weekend on the plot.
+        
+        Parameter
+        ---------
+        color : str
+            color to use for the weekends
+        """
+        first_sat = copy(self.extrema.min)
+        while first_sat.weekday() != 5:
+            first_sat += datetime.timedelta(days=1)
+        ybound = 1.1 * len(self.ykeys)
+        this_date = copy(first_sat)
+        while this_date < self.extrema.max:
+            plt.fill_between([this_date, this_date + datetime.timedelta(days=2)], [ybound, ybound], -10, color=color)
+            this_date += datetime.timedelta(days=7)
+
+    def assign_yvals_labels(self):
+        """
+        Assigned a yvalue to all ykeys (colinear thing...)
+        """
+        step = 0.5
+        ymin = step
+        self.yvals = []
+        fnd_keys = {}
+        ctr = 0
+        self.yticks = []
+        self.ylabels = []
+        cmap = {}
+        for i, ykey in enumerate(self.ykeys):
+            if ykey not in fnd_keys:
+                self.ylabels.append(self.labels[i])
+                yval = ymin + ctr * step
+                self.yticks.append(yval)
+                cmap[ykey] = ctr
+                ctr += 1
+                self.yvals.append(yval)
+                fnd_keys[ykey] = yval
+            else:
+                self.yvals.append(fnd_keys[ykey])
+                self.ylabels[cmap[ykey]] += '|' + self.labels[i]
+            self.ylabels[cmap[ykey]] = self.ylabels[cmap[ykey]].strip('| ')
+
+    def date_ticks(self, interval):
+        interval_mmap = {1:1, 2:2, 3:3, 4:3, 5:6, 7:6, 8:6, 9:12, 10:12, 11:12}
+        dyr = self.deltadate / 365.0
+        self.itvmapper = matplotlib.dates.MONTHLY
+        if interval is not None:
+            try:
+                interval = float(interval)
+            except ValueError:
+                if interval.endswith('d'):
+                    self.itvmapper = matplotlib.dates.DAILY
+                    interval = float(interval.strip('d'))
+                elif interval.endswith('m'):
+                    interval = float(interval.strip('m'))
+                else:
+                    raise ValueError(f"Interval must be float[d/m], you provided {interval}")
+        else:
+            if self.deltadate > 300:
+                interval = interval_mmap[int(np.ceil(dyr * dyr / 6.0))]
+                self.fmttr = "%b '%y"
+            else:
+                self.itvmapper = matplotlib.dates.DAILY
+                if self.deltadate > 30:
+                    interval = 7
+                else:
+                    interval = 1
+                self.fmttr = "(%a) %b/%d"
+        self.interval = interval
+
+    def chart(self, **kwargs):
+        """
+        This will plot a gantt chart of items (ylabels) and dates.  If included, it will plot percent
+        complete for tasks and color code for milestones (note, if included, it must have a status_codes
+        entry for every label)  If included, it will connect predecessors (note, if included, it also
+        must have an entry for every ylabel) other_labels prints another label by the entry (to right on
+        plot), it also must have an entry for every ylabel
+
+        Parameters
+        ----------
+
+        """
+        # Initialise plot
+        fig1 = plt.figure(figsize=(12, 8), tight_layout=True)
+        ax1 = fig1.add_subplot(111)
+        ax1.axis(xmin=matplotlib.dates.date2num(self.extrema.min)-self.deltadate/10.0,
+                 xmax=matplotlib.dates.date2num(self.extrema.max)+self.deltadate/10.0)
+        self.assign_yvals_labels()
+        step = self.yticks[1] - self.yticks[0]
+        show_weekends = False if 'show_weekends' not in kwargs else kwargs['show_weekends']
+        if show_weekends:
+            self.plot_weekends()
+
+        # Plot the data
+        for i, dtlim in enumerate(self.dates):
+            pp = self.plotpars[i]
+            start = matplotlib.dates.date2num(dtlim[0])
+            if dtlim[1] is None:  # Milestone
+                plt.plot(start, self.yvals[i], pp.marker, color=pp.color, markersize=8)
+            else:
+                stop = matplotlib.dates.date2num(dtlim[1])
+                plt.barh(self.yvals[i],  stop - start, left=start, height=0.3, align='center', color=pp.color, alpha=0.75)
+                if isinstance(pp.status, (float, int)):
+                    plt.barh(self.yvals[i], pp.status*(stop - start)/100.0, left=start, height=0.1, align='center', color='k', alpha=0.75)
+
+        # Format the y-axis
+        locsy, labelsy = plt.yticks(self.yticks, self.ylabels)
+        plt.setp(labelsy, fontsize=14)
+        show_grid = False if 'grid' not in kwargs else kwargs['grid']
+        if show_grid:
+            plt.grid(color='0.6', linestyle=':')
+
+        # Plot current time
+        now = datetime.datetime.now()
+        if now >= self.extrema.min and now <= self.extrema.max:
+            now_num = matplotlib.dates.date2num(now)
+            plt.plot([now_num, now_num], [self.yticks[0]-step, self.yticks[-1]+step], '--', color=color_palette[3])
+        if int(self.deltadate) > 400:  # plot year markers
+            yr1 = self.extrema.min.year
+            yr2 = self.extrema.max.year
+            if self.deltadate > 2.5*365 and self.extrema.max.month > 8:
+                yr2 += 1
+            for yr in range(yr1, yr2+1):
+                this_yr = datetime.datetime(year=yr, month=1, day=1)
+                plt.plot([this_yr, this_yr], [self.yticks[0]-step, self.yticks[-1]+step], 'k:')
+        ax1.xaxis_date()  # Tell matplotlib that these are dates...
+        interval = None if 'interval' not in kwargs else kwargs['interval']
+        self.date_ticks(interval)
+        rule = matplotlib.dates.rrulewrapper(self.itvmapper, interval=self.interval)
+        loc = matplotlib.dates.RRuleLocator(rule)
+        formatter = matplotlib.dates.DateFormatter(self.fmttr)
+        ax1.xaxis.set_major_locator(loc)
+        ax1.xaxis.set_major_formatter(formatter)
+        labelsx = ax1.get_xticklabels()
+        plt.setp(labelsx, rotation=30, fontsize=12)
+
+        # Finish up
+        ax1.invert_yaxis()
+        ax1.axis(ymin=self.yticks[-1] + (step - 0.01), ymax=self.yticks[0] - (step - 0.01))
+        fig1.autofmt_xdate()
+        plt.tight_layout()
+
+def cumulative_graph(dates, values, norm):
+    datemin, datemax = matplotlib.dates.date2num(dates[0]), matplotlib.dates.date2num(dates[-1])
+    cx_dat = [matplotlib.dates.date2num(_x) for _x in dates]
+    cy_dat = np.array(values) / norm
+    fig2 = plt.figure('cdf')
+    ax2 = fig2.add_subplot(111)
+    ax2.axis(xmin=datemin, xmax=datemax, ymin=0.0, ymax=1.0)
+    plt.plot(cx_dat, cy_dat)
+    plt.ylabel('Fraction Completed')
+    plt.grid()
+    ax2.xaxis_date()  # Tell matplotlib that these are dates...
+    rule = matplotlib.dates.rrulewrapper(matplotlib.dates.MONTHLY, interval=1)
+    loc = matplotlib.dates.RRuleLocator(rule)
+    formatter = matplotlib.dates.DateFormatter("%b '%y")
+    ax2.xaxis.set_major_locator(loc)
+    ax2.xaxis.set_major_formatter(formatter)
+    labelsx = ax2.get_xticklabels()
+    plt.setp(labelsx, rotation=30, fontsize=12)
