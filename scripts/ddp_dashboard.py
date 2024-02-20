23b394a997bfe90379e8278dfab3eba47ca1346a
DAVID DEBOER <ddeboer@berkeley.edu>
2024-02-19 21:58:03 -0800

    update to manager

--- scripts/ddp_dashboard.py
+++ scripts/ddp_dashboard.py
@@ -7,7 +7,7 @@ ap.add_argument('yaml', help="Name of input yaml file")
 ap.add_argument('-r', '--report', help="Flag to write report.", action='store_true')
 args = ap.parse_args()
 
-mgr = manager.Finance(args.yaml)
+mgr = manager.Manager(args.yaml)
 mgr.dashboard(report=args.report)
 manager.plot.plt.show()
 
